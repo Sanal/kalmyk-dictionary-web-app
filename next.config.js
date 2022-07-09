26a1a23192cb0e49cf8edf37f7331ffbf446b124
Sanal Tserenov <sanaltserenov@outlook.com>
2022-07-09 08:44:22 +0300

    build: add storybook

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    config.resolve.fallback = { util: require.resolve('util/') };
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
